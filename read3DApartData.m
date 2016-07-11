e36b9bed4e3c88cbf1a3f105e2b749d2205ffbc9
Qing Wang <wangqing@stanford.edu>
2016-07-10 21:57:29 -0700

    updated LPT reader for Chemkin version

--- read3DApartData.m
+++ read3DApartData.m
@@ -1,4 +1,4 @@
-function read3DApartData(filename)
+function read3DApartData(filename,version)
 fid = fopen(filename);
 npart = fread(fid,1,'integer*4');
 part_size = fread(fid,1,'integer*4');
@@ -7,15 +7,32 @@ time = fread(fid,1,'real*8');
 ref = {'x','y','z','u','v','w','m','d','rho','T','dmdt','beta','tbreak', ...
     'nparcel','dt','i','j','k','tag','stop'};
 part = zeros(npart,length(ref));
-for ipart = 1:npart
-    for ii = 1:15
-        part(ipart,ii) = fread(fid,1,'real*8');
-    end
-    for ii = 16:length(ref)
-        part(ipart,ii) = fread(fid,1,'integer*4');
-    end
+switch version
+    case 'FlameMaster 3DA'
+        for ipart = 1:npart
+            for ii = 1:15
+                part(ipart,ii) = fread(fid,1,'real*8');
+            end
+            for ii = 16:length(ref)
+                part(ipart,ii) = fread(fid,1,'integer*4');
+            end
+        end
+        fclose(fid);
+        save('./part_data.mat','npart','part_size','dt','time','ref','part')
+    case 'Chemkin 3DA'
+        max_comp = 10;
+        mk = zeros(npart,max_comp);
+        for ipart = 1:npart
+            for ii = 1:15
+                part(ipart,ii) = fread(fid,1,'real*8');
+            end
+            for ii = 1:max_comp
+                mk(ipart,ii) = fread(fid,1,'real*8');
+            end
+            for ii = 16:length(ref)
+                part(ipart,ii) = fread(fid,1,'integer*4');
+            end
+        end
+        fclose(fid);
+        save('./part_data.mat','npart','part_size','dt','time','ref','part','max_comp','mk')
 end
-
-fclose(fid);
-
-save('./part_data.mat','npart','part_size','dt','time','ref','part')
