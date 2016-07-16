8e56e497750fb324ae19b4b860c70a6e7f068832
Qing Wang <wangqing@stanford.edu>
2016-07-16 14:08:59 -0700

    bug fixed

--- write3DApartData.m
+++ write3DApartData.m
@@ -6,17 +6,17 @@ fwrite(fid,data.dt,'real*8');
 fwrite(fid,data.time,'real*8');
 switch version
     case 'FlameMaster 3DA'
-        for ipart = 1:npart
+        for ipart = 1:data.npart
             fwrite(fid,data.part(ipart,1:15),'real*8');
             fwrite(fid,data.part(ipart,16:length(data.ref)),'integer*4');
         end
     case 'Chemkin 3DA'
         max_comp = size(data.part,2);
-        for ipart = 1:npart
-                fwrite(fid,data.part(ipart,1:7),'real*8');
-                fwrite(fid,data.mk(ipart,1:max_comp),'real*8');
-                fwrite(fid,data.part(ipart,8:15),'real*8');
-                fwrite(fid,data.part(ipart,16:length(ref)),'integer*4');
+        for ipart = 1:data.npart
+            fwrite(fid,data.part(ipart,1:7),'real*8');
+            fwrite(fid,data.mk(ipart,1:max_comp),'real*8');
+            fwrite(fid,data.part(ipart,8:15),'real*8');
+            fwrite(fid,data.part(ipart,16:length(ref)),'integer*4');
         end
 end
 fclose(fid);
