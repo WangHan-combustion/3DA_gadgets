21352315a6eb5613537b228694f732698306e420
Qing Wang <wangqing@stanford.edu>
2016-08-30 16:34:39 -0700

    modified particle reader for chemkin 3DA

--- read3DApartData.m
+++ read3DApartData.m
@@ -4,11 +4,11 @@ npart = fread(fid,1,'integer*4');
 part_size = fread(fid,1,'integer*4');
 dt = fread(fid,1,'real*8');
 time = fread(fid,1,'real*8');
-ref = {'x','y','z','u','v','w','m','d','rho','T','dmdt','beta','tbreak', ...
-    'nparcel','dt','i','j','k','tag','stop'};
-part = zeros(npart,length(ref));
 switch version
     case 'FlameMaster 3DA'
+        ref = {'x','y','z','u','v','w','m','d','rho','T','dmdt','beta','tbreak', ...
+            'nparcel','dt','i','j','k','tag','stop'};
+        part = zeros(npart,length(ref));
         for ipart = 1:npart
             for ii = 1:15
                 part(ipart,ii) = fread(fid,1,'real*8');
@@ -20,6 +20,23 @@ switch version
         fclose(fid);
         save('./part_data.mat','npart','part_size','dt','time','ref','part')
     case 'Chemkin 3DA'
+        ref = {'x','y','z','u','v','w','m','d','rho','T','dmdt','beta','tbreak', ...
+            'nparcel','dt','del_u','del_v','del_w','i','j','k','tag','stop'};
+        part = zeros(npart,length(ref));
+        for ipart = 1:npart
+            for ii = 1:18
+                part(ipart,ii) = fread(fid,1,'real*8');
+            end
+            for ii = 19:length(ref)
+                part(ipart,ii) = fread(fid,1,'integer*4');
+            end
+        end
+        fclose(fid);
+        save('./part_data.mat','npart','part_size','dt','time','ref','part')
+    case 'Chemkin 3DA Multicomp'
+        ref = {'x','y','z','u','v','w','m','d','rho','T','dmdt','beta','tbreak', ...
+            'nparcel','dt','i','j','k','tag','stop'};
+        part = zeros(npart,length(ref));
         max_comp = 10;
         mk = zeros(npart,max_comp);
         for ipart = 1:npart
