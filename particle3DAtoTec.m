0c03c2d783ce4cddfc3123ac1ee28ea4d1b2a104
Qing Wang <wangqing@stanford.edu>
2016-10-28 11:36:03 -0700

    add sampling to particle3DAtoTec

--- particle3DAtoTec.m
+++ particle3DAtoTec.m
@@ -1,4 +1,4 @@
-function particle3DAtoTec(part_data,filename)
+function particle3DAtoTec(part_data,filename,nskip)
 fid = fopen([filename,'.dat'],'w');
 % print the name of variables
 fprintf(fid,'variables = ');
@@ -11,9 +11,13 @@ fprintf(fid,'zone t = "droplet"\n');
 % print the data type
 fprintf(fid,'f = point\n');
 % print the data
-n = size(part_data.part,1);
+if (narginxi<3)
+  nskip = 1;
+end
+n_tot = size(part_data.part,1);
+n = floor(n_tot/nskip);
 fprintf(fid,'i = %i, j = %i, k = %i\n',n,1,1);
-for ii = 1:n
+for ii = 1:nskip:n_tot
     for ivar = 1:length(part_data.ref)
         fprintf(fid,'%15.7f ',part_data.part(ii,ivar));
     end
